313c93abf00f63e43cee7c281b98559566084a8d
micsoni <micsoni@gmail.com>
2020-03-09 10:40:51 +0100

    added index.js file of express app

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+//const logingRouter = require("./auth/router");
+//const userRouter = require("./user/router")
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+//using cors
+const corsMiddleware = cors();
+app.use(corsMiddleware);
+
+//using bodyparser from express
+const parser = express.json();
+app.use(parser);
+
+// using jwt
+//app.use(logingRouter);
+
+//routers for endpoints
+//app.use(userRouter)
+
+app.listen(port, () => console.log(`Listening on :${port}`));
